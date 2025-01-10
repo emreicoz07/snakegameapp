5ac81cf431218bfcc0458399e3013238a43fce2c
Ramazan Emre ICOZ <106469815+emreicoz07@users.noreply.github.com>
2025-01-10 19:21:11 +0100

    Delete .history directory

--- .history/constants/game_20250108225026.ts
+++
@@ -1,8 +0,0 @@
-export const GRID_SIZE = 15;
-export const CELL_SIZE = 20;
-export const GAME_SPEED = 100; // milisaniye
-export const INITIAL_SNAKE_POSITION = [
-  { x: 5, y: 5 },
-  { x: 4, y: 5 },
-  { x: 3, y: 5 },
-]; 
